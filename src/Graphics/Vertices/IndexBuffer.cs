77485076f6398292d91afc9fd00cc73704f23a3f
Ethan Lee <flibitijibibo@gmail.com>
2016-02-08 23:14:24 -0500

    Remove GraphicsProfile check

--- src/Graphics/Vertices/IndexBuffer.cs
+++ src/Graphics/Vertices/IndexBuffer.cs
@@ -296,13 +296,6 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 			if (sizeInBytes == 4)
 			{
-				if (graphicsDevice.GraphicsProfile == GraphicsProfile.Reach)
-				{
-					throw new NotSupportedException(
-						"The profile does not support an elementSize of IndexElementSize.ThirtyTwoBits; " +
-						"use IndexElementSize.SixteenBits or a type that has a size of two bytes."
-					);
-				}
 				return IndexElementSize.ThirtyTwoBits;
 			}
 
