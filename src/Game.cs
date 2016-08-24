2602ed69e5d60e3c4a67bf6bb6464eb389b50d34
Ethan Lee <flibitijibibo@gmail.com>
2016-08-24 12:28:05 -0400

    Store IUpdateable/IDrawable like XNA seems to (blech)

--- src/Game.cs
+++ src/Game.cs
@@ -200,25 +200,11 @@ namespace Microsoft.Xna.Framework
 
 		#region Private Variables
 
-		private SortingFilteringCollection<IDrawable> _drawables =
-			new SortingFilteringCollection<IDrawable>(
-				d => d.Visible,
-				(d, handler) => d.VisibleChanged += handler,
-				(d, handler) => d.VisibleChanged -= handler,
-				(d1, d2) => Comparer<int>.Default.Compare(d1.DrawOrder, d2.DrawOrder),
-				(d, handler) => d.DrawOrderChanged += handler,
-				(d, handler) => d.DrawOrderChanged -= handler
-			);
+		private List<IUpdateable> updateableComponents;
+		private List<IUpdateable> currentlyUpdatingComponents;
 
-		private SortingFilteringCollection<IUpdateable> _updateables =
-			new SortingFilteringCollection<IUpdateable>(
-				u => u.Enabled,
-				(u, handler) => u.EnabledChanged += handler,
-				(u, handler) => u.EnabledChanged -= handler,
-				(u1, u2) => Comparer<int>.Default.Compare(u1.UpdateOrder, u2.UpdateOrder),
-				(u, handler) => u.UpdateOrderChanged += handler,
-				(u, handler) => u.UpdateOrderChanged -= handler
-			);
+		private List<IDrawable> drawableComponents;
+		private List<IDrawable> currentlyDrawingComponents;
 
 		private IGraphicsDeviceService graphicsDeviceService;
 		private bool hasInitialized;
@@ -264,6 +250,11 @@ namespace Microsoft.Xna.Framework
 			Components = new GameComponentCollection();
 			Services = new GameServiceContainer();
 			Content = new ContentManager(Services);
+
+			updateableComponents = new List<IUpdateable>();
+			currentlyUpdatingComponents = new List<IUpdateable>();
+			drawableComponents = new List<IDrawable>();
+			currentlyDrawingComponents = new List<IDrawable>();
 
 			IsActive = true;
 			IsMouseVisible = false;
@@ -326,7 +317,6 @@ namespace Microsoft.Xna.Framework
 					{
 						Content.Dispose();
 					}
-
 
 					if (graphicsDeviceService != null)
 					{
@@ -718,24 +708,35 @@ namespace Microsoft.Xna.Framework
 			}
 		}
 
-		private static readonly Action<IDrawable, GameTime> DrawAction =
-			(drawable, gameTime) => drawable.Draw(gameTime);
-
 		protected virtual void Draw(GameTime gameTime)
 		{
-			_drawables.ForEachFilteredItem(DrawAction, gameTime);
+			currentlyDrawingComponents.AddRange(drawableComponents);
+			foreach (IDrawable drawable in currentlyDrawingComponents)
+			{
+				if (drawable.Visible)
+				{
+					drawable.Draw(gameTime);
+				}
+			}
+			currentlyDrawingComponents.Clear();
 		}
-
-		private static readonly Action<IUpdateable, GameTime> UpdateAction =
-			(updateable, gameTime) => updateable.Update(gameTime);
 
 		protected virtual void Update(GameTime gameTime)
 		{
 #if BASIC_PROFILER
 			updateStart = gameTimer.ElapsedTicks;
 #endif
+			currentlyUpdatingComponents.AddRange(updateableComponents);
+			foreach (IUpdateable updateable in currentlyUpdatingComponents)
+			{
+				if (updateable.Enabled)
+				{
+					updateable.Update(gameTime);
+				}
+			}
+			currentlyUpdatingComponents.Clear();
+
 			FrameworkDispatcher.Update();
-			_updateables.ForEachFilteredItem(UpdateAction, gameTime);
 #if BASIC_PROFILER
 			updateTime = gameTimer.ElapsedTicks - updateStart;
 #endif
@@ -810,14 +811,14 @@ namespace Microsoft.Xna.Framework
 			 * lists synced and to Initialize future components as they are
 			 * added.
 			 */
-			_updateables.Clear();
-			_drawables.Clear();
+			updateableComponents.Clear();
+			drawableComponents.Clear();
 			for (int i = 0; i < Components.Count; i += 1)
 			{
 				CategorizeComponent(Components[i]);
 			}
-			Components.ComponentAdded += Components_ComponentAdded;
-			Components.ComponentRemoved += Components_ComponentRemoved;
+			Components.ComponentAdded += OnComponentAdded;
+			Components.ComponentRemoved += OnComponentRemoved;
 		}
 
 		private void CategorizeComponent(IGameComponent component)
@@ -825,14 +826,42 @@ namespace Microsoft.Xna.Framework
 			IUpdateable updateable = component as IUpdateable;
 			if (updateable != null)
 			{
-				_updateables.Add(updateable);
+				SortUpdateable(updateable);
+				updateable.UpdateOrderChanged += OnUpdateOrderChanged;
 			}
 
 			IDrawable drawable = component as IDrawable;
 			if (drawable != null)
 			{
-				_drawables.Add(drawable);
+				SortDrawable(drawable);
+				drawable.DrawOrderChanged += OnDrawOrderChanged;
 			}
+		}
+
+		private void SortUpdateable(IUpdateable updateable)
+		{
+			for (int i = 0; i < updateableComponents.Count; i += 1)
+			{
+				if (updateable.UpdateOrder < updateableComponents[i].UpdateOrder)
+				{
+					updateableComponents.Insert(i, updateable);
+					return;
+				}
+			}
+			updateableComponents.Add(updateable);
+		}
+
+		private void SortDrawable(IDrawable drawable)
+		{
+			for (int i = 0; i < drawableComponents.Count; i += 1)
+			{
+				if (drawable.DrawOrder < drawableComponents[i].DrawOrder)
+				{
+					drawableComponents.Insert(i, drawable);
+					return;
+				}
+			}
+			drawableComponents.Add(drawable);
 		}
 
 		private GraphicsDevice InitializeGraphicsService()
@@ -855,7 +884,7 @@ namespace Microsoft.Xna.Framework
 
 		#region Private Event Handlers
 
-		private void Components_ComponentAdded(
+		private void OnComponentAdded(
 			object sender,
 			GameComponentCollectionEventArgs e
 		) {
@@ -866,21 +895,39 @@ namespace Microsoft.Xna.Framework
 			CategorizeComponent(e.GameComponent);
 		}
 
-		private void Components_ComponentRemoved(
+		private void OnComponentRemoved(
 			object sender,
 			GameComponentCollectionEventArgs e
 		) {
 			IUpdateable updateable = e.GameComponent as IUpdateable;
 			if (updateable != null)
 			{
-				_updateables.Remove(updateable);
+				updateableComponents.Remove(updateable);
+				updateable.UpdateOrderChanged -= OnUpdateOrderChanged;
 			}
 
 			IDrawable drawable = e.GameComponent as IDrawable;
 			if (drawable != null)
 			{
-				_drawables.Remove(drawable);
+				drawableComponents.Remove(drawable);
+				drawable.DrawOrderChanged -= OnDrawOrderChanged;
 			}
+		}
+
+		private void OnUpdateOrderChanged(object sender, EventArgs e)
+		{
+			// FIXME: Is there a better way to re-sort one item? -flibit
+			IUpdateable updateable = sender as IUpdateable;
+			updateableComponents.Remove(updateable);
+			SortUpdateable(updateable);
+		}
+
+		private void OnDrawOrderChanged(object sender, EventArgs e)
+		{
+			// FIXME: Is there a better way to re-sort one item? -flibit
+			IDrawable drawable = sender as IDrawable;
+			drawableComponents.Remove(drawable);
+			SortDrawable(drawable);
 		}
 
 		private void OnUnhandledException(
@@ -888,320 +935,6 @@ namespace Microsoft.Xna.Framework
 			UnhandledExceptionEventArgs args
 		) {
 			ShowMissingRequirementMessage(args.ExceptionObject as Exception);
-		}
-
-		#endregion
-
-		#region SortingFilteringCollection class
-
-		/// <summary>
-		/// The SortingFilteringCollection class provides efficient, reusable
-		/// sorting and filtering based on a configurable sort comparer, filter
-		/// predicate, and associate change events.
-		/// </summary>
-		class SortingFilteringCollection<T> : ICollection<T>
-		{
-			private readonly List<T> _items;
-			private readonly List<AddJournalEntry<T>> _addJournal;
-			private readonly Comparison<AddJournalEntry<T>> _addJournalSortComparison;
-			private readonly List<int> _removeJournal;
-			private readonly List<T> _cachedFilteredItems;
-			private bool _shouldRebuildCache;
-
-			private readonly Predicate<T> _filter;
-			private readonly Comparison<T> _sort;
-			private readonly Action<T, EventHandler<EventArgs>> _filterChangedSubscriber;
-			private readonly Action<T, EventHandler<EventArgs>> _filterChangedUnsubscriber;
-			private readonly Action<T, EventHandler<EventArgs>> _sortChangedSubscriber;
-			private readonly Action<T, EventHandler<EventArgs>> _sortChangedUnsubscriber;
-
-			public SortingFilteringCollection(
-				Predicate<T> filter,
-				Action<T, EventHandler<EventArgs>> filterChangedSubscriber,
-				Action<T, EventHandler<EventArgs>> filterChangedUnsubscriber,
-				Comparison<T> sort,
-				Action<T, EventHandler<EventArgs>> sortChangedSubscriber,
-				Action<T, EventHandler<EventArgs>> sortChangedUnsubscriber
-			) {
-				_items = new List<T>();
-				_addJournal = new List<AddJournalEntry<T>>();
-				_removeJournal = new List<int>();
-				_cachedFilteredItems = new List<T>();
-				_shouldRebuildCache = true;
-
-				_filter = filter;
-				_filterChangedSubscriber = filterChangedSubscriber;
-				_filterChangedUnsubscriber = filterChangedUnsubscriber;
-				_sort = sort;
-				_sortChangedSubscriber = sortChangedSubscriber;
-				_sortChangedUnsubscriber = sortChangedUnsubscriber;
-
-				_addJournalSortComparison = CompareAddJournalEntry;
-			}
-
-			private int CompareAddJournalEntry(AddJournalEntry<T> x, AddJournalEntry<T> y)
-			{
-				int result = _sort(x.Item, y.Item);
-				if (result != 0)
-				{
-					return result;
-				}
-				return x.Order - y.Order;
-			}
-
-			public void ForEachFilteredItem<TUserData>(
-				Action<T, TUserData> action,
-				TUserData userData
-			) {
-				if (_shouldRebuildCache)
-				{
-					ProcessRemoveJournal();
-					ProcessAddJournal();
-
-					// Rebuild the cache.
-					_cachedFilteredItems.Clear();
-					for (int i = 0; i < _items.Count; i += 1)
-						if (_filter(_items[i]))
-						{
-							_cachedFilteredItems.Add(_items[i]);
-						}
-
-					_shouldRebuildCache = false;
-				}
-
-				for (int i = 0; i < _cachedFilteredItems.Count; i += 1)
-				{
-					action(_cachedFilteredItems[i], userData);
-				}
-
-				/* If the cache was invalidated as a result of processing items,
-				 * now is a good time to clear it and give the GC (more of) a
-				 * chance to do its thing.
-				 */
-				if (_shouldRebuildCache)
-				{
-					_cachedFilteredItems.Clear();
-				}
-			}
-
-			public void Add(T item)
-			{
-				/* NOTE: We subscribe to item events after items in _addJournal
-				 * have been merged.
-				 */
-				_addJournal.Add(new AddJournalEntry<T>(_addJournal.Count, item));
-				InvalidateCache();
-			}
-
-			public bool Remove(T item)
-			{
-				if (_addJournal.Remove(AddJournalEntry<T>.CreateKey(item)))
-				{
-					return true;
-				}
-
-				int index = _items.IndexOf(item);
-				if (index >= 0)
-				{
-					UnsubscribeFromItemEvents(item);
-					_removeJournal.Add(index);
-					InvalidateCache();
-					return true;
-				}
-				return false;
-			}
-
-			public void Clear()
-			{
-				for (int i = 0; i < _items.Count; i += 1)
-				{
-					_filterChangedUnsubscriber(_items[i], Item_FilterPropertyChanged);
-					_sortChangedUnsubscriber(_items[i], Item_SortPropertyChanged);
-				}
-
-				_addJournal.Clear();
-				_removeJournal.Clear();
-				_items.Clear();
-
-				InvalidateCache();
-			}
-
-			public bool Contains(T item)
-			{
-				return _items.Contains(item);
-			}
-
-			public void CopyTo(T[] array, int arrayIndex)
-			{
-				_items.CopyTo(array, arrayIndex);
-			}
-
-			public int Count
-			{
-				get
-				{
-					return _items.Count;
-				}
-			}
-
-			public bool IsReadOnly
-			{
-				get
-				{
-					return false;
-				}
-			}
-
-			public IEnumerator<T> GetEnumerator()
-			{
-				return _items.GetEnumerator();
-			}
-
-			System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-			{
-				return ((System.Collections.IEnumerable) _items).GetEnumerator();
-			}
-
-			// Sort high to low.
-			private static readonly Comparison<int> RemoveJournalSortComparison =
-				(x, y) => Comparer<int>.Default.Compare(y, x);
-
-			private void ProcessRemoveJournal()
-			{
-				if (_removeJournal.Count == 0)
-				{
-					return;
-				}
-
-				/* Remove items in reverse. (Technically there exist faster
-				 * ways to bulk-remove from a variable-length array, but List<T>
-				 * does not provide such a method.)
-				 */
-				_removeJournal.Sort(RemoveJournalSortComparison);
-				for (int i = 0; i < _removeJournal.Count; i += 1)
-				{
-					_items.RemoveAt(_removeJournal[i]);
-				}
-				_removeJournal.Clear();
-			}
-
-			private void ProcessAddJournal()
-			{
-				if (_addJournal.Count == 0)
-				{
-					return;
-				}
-
-				/* Prepare the _addJournal to be merge-sorted with _items.
-				 * _items is already sorted (because it is always sorted).
-				 */
-				_addJournal.Sort(_addJournalSortComparison);
-
-				int iAddJournal = 0;
-				int iItems = 0;
-
-				while (iItems < _items.Count && iAddJournal < _addJournal.Count)
-				{
-					T addJournalItem = _addJournal[iAddJournal].Item;
-					/* If addJournalItem is less than (belongs before)
-					 * _items[iItems], insert it.
-					 */
-					if (_sort(addJournalItem, _items[iItems]) < 0)
-					{
-						SubscribeToItemEvents(addJournalItem);
-						_items.Insert(iItems, addJournalItem);
-						iAddJournal += 1;
-					}
-					/* Always increment iItems, either because we inserted and
-					 * need to move past the insertion, or because we didn't
-					 * insert and need to consider the next element.
-					 */
-					iItems += 1;
-				}
-
-				// If _addJournal had any "tail" items, append them all now.
-				for (; iAddJournal < _addJournal.Count; iAddJournal += 1)
-				{
-					T addJournalItem = _addJournal[iAddJournal].Item;
-					SubscribeToItemEvents(addJournalItem);
-					_items.Add(addJournalItem);
-				}
-
-				_addJournal.Clear();
-			}
-
-			private void SubscribeToItemEvents(T item)
-			{
-				_filterChangedSubscriber(item, Item_FilterPropertyChanged);
-				_sortChangedSubscriber(item, Item_SortPropertyChanged);
-			}
-
-			private void UnsubscribeFromItemEvents(T item)
-			{
-				_filterChangedUnsubscriber(item, Item_FilterPropertyChanged);
-				_sortChangedUnsubscriber(item, Item_SortPropertyChanged);
-			}
-
-			private void InvalidateCache()
-			{
-				_shouldRebuildCache = true;
-			}
-
-			private void Item_FilterPropertyChanged(object sender, EventArgs e)
-			{
-				InvalidateCache();
-			}
-
-			private void Item_SortPropertyChanged(object sender, EventArgs e)
-			{
-				T item = (T)sender;
-				int index = _items.IndexOf(item);
-
-				_addJournal.Add(new AddJournalEntry<T>(_addJournal.Count, item));
-				_removeJournal.Add(index);
-
-				/* Until the item is back in place, we don't care about its
-				 * events. We will re-subscribe when _addJournal is processed.
-				 */
-				UnsubscribeFromItemEvents(item);
-				InvalidateCache();
-			}
-		}
-
-		#endregion
-
-		#region AddJournalEntry struct
-
-		private struct AddJournalEntry<T>
-		{
-			public readonly int Order;
-			public readonly T Item;
-
-			public AddJournalEntry(int order, T item)
-			{
-				Order = order;
-				Item = item;
-			}
-
-			public static AddJournalEntry<T> CreateKey(T item)
-			{
-				return new AddJournalEntry<T>(-1, item);
-			}
-
-			public override int GetHashCode()
-			{
-				return Item.GetHashCode();
-			}
-
-			public override bool Equals(object obj)
-			{
-				if (!(obj is AddJournalEntry<T>))
-				{
-					return false;
-				}
-
-				return object.Equals(Item, ((AddJournalEntry<T>) obj).Item);
-			}
 		}
 
 		#endregion
