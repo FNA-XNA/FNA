ce0b1f3ac87a76fe67ae31acd703a4c4cca872fb
Ethan Lee <flibitijibibo@gmail.com>
2017-10-12 17:33:31 -0400

    Buildfix

--- src/Game.cs
+++ src/Game.cs
@@ -37,6 +37,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
 #endregion
 
 namespace Microsoft.Xna.Framework
