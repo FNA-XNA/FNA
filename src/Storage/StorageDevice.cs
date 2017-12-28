2d6cf2139731babf72ca79746ef9e9b67d33d639
Ethan Lee <flibitijibibo@gmail.com>
2017-12-27 19:19:32 -0500

    Fix /foo vs. /fooo case (thanks Egorbo!)

--- src/Storage/StorageDevice.cs
+++ src/Storage/StorageDevice.cs
@@ -377,6 +377,11 @@ namespace Microsoft.Xna.Framework.Storage
 			string[] drives = Environment.GetLogicalDrives();
 			for (int i = 0; i < drives.Length; i += 1)
 			{
+				if (	!string.IsNullOrEmpty(drives[i]) &&
+					drives[i][drives[i].Length - 1] != Path.DirectorySeparatorChar	)
+				{
+					drives[i] += Path.DirectorySeparatorChar;
+				}
 				if (	storageRoot.StartsWith(drives[i]) &&
 					drives[i].Length > length	)
 				{
