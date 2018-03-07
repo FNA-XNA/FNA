3d18f899f0cb94a3afd752d66e68bc5f0d95da2c
Ethan Lee <flibitijibibo@gmail.com>
2018-03-07 12:28:51 -0500

    samplerMap can just use the pointer value (update MojoShader!)

--- src/Graphics/Effect/Effect.cs
+++ src/Graphics/Effect/Effect.cs
@@ -58,7 +58,20 @@ namespace Microsoft.Xna.Framework.Graphics
 
 		#region Private Variables
 
-		private Dictionary<string, EffectParameter> samplerMap = new Dictionary<string, EffectParameter>();
+		private Dictionary<IntPtr, EffectParameter> samplerMap = new Dictionary<IntPtr, EffectParameter>(new IntPtrBoxlessComparer());
+		private class IntPtrBoxlessComparer : IEqualityComparer<IntPtr>
+		{
+			public bool Equals(IntPtr x, IntPtr y)
+			{
+				return x == y;
+			}
+
+			public int GetHashCode(IntPtr obj)
+			{
+				return obj.GetHashCode();
+			}
+		}
+
 		private IntPtr stateChangesPtr;
 
 		#endregion
@@ -847,12 +860,9 @@ namespace Microsoft.Xna.Framework.Graphics
 					MojoShader.MOJOSHADER_samplerStateType type = states[j].type;
 					if (type == MojoShader.MOJOSHADER_samplerStateType.MOJOSHADER_SAMP_TEXTURE)
 					{
-						string samplerName = Marshal.PtrToStringAnsi(
-							registers[i].sampler_name
-						);
-						if (samplerMap.ContainsKey(samplerName))
+						if (samplerMap.ContainsKey(registers[i].sampler_name))
 						{
-							Texture texture = samplerMap[samplerName].texture;
+							Texture texture = samplerMap[registers[i].sampler_name].texture;
 							if (texture != null)
 							{
 								textures[register] = texture;
@@ -1082,7 +1092,7 @@ namespace Microsoft.Xna.Framework.Graphics
 					{
 						if (textureName.Equals(parameters[j].Name))
 						{
-							samplerMap[Marshal.PtrToStringAnsi(param.value.name)] = parameters[j];
+							samplerMap[param.value.name] = parameters[j];
 							break;
 						}
 					}
