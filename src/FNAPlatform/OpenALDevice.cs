aa8d89216673963189dbb41643a7c45649a37f60
Ethan Lee <flibitijibibo@gmail.com>
2018-03-07 11:39:23 -0500

    Use fixed keyword for StereoToMono conversion (thanks @bartwe!)

--- src/FNAPlatform/OpenALDevice.cs
+++ src/FNAPlatform/OpenALDevice.cs
@@ -403,7 +403,7 @@ namespace Microsoft.Xna.Framework.Audio
 #endif
 		}
 
-		public IALBuffer ConvertStereoToMono(IALBuffer buffer)
+		public unsafe IALBuffer ConvertStereoToMono(IALBuffer buffer)
 		{
 			OpenALBuffer buf = buffer as OpenALBuffer;
 			int bufLen, bits;
@@ -423,25 +423,22 @@ namespace Microsoft.Xna.Framework.Audio
 #endif
 
 			byte[] data = new byte[bufLen];
-			GCHandle dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
-			IntPtr dataPtr = dataHandle.AddrOfPinnedObject();
-			ALEXT.alGetBufferSamplesSOFT(
-				buf.Handle,
-				0,
-				bufLen / bits / 2,
-				ALEXT.AL_STEREO_SOFT,
-				bits == 2 ? ALEXT.AL_SHORT_SOFT : ALEXT.AL_BYTE_SOFT,
-				dataPtr
-			);
+			byte[] monoData = new byte[bufLen / 2];
+			fixed (byte* dataPtr = &data[0])
+			fixed (byte* monoPtr = &monoData[0])
+			{
+				ALEXT.alGetBufferSamplesSOFT(
+					buf.Handle,
+					0,
+					bufLen / bits / 2,
+					ALEXT.AL_STEREO_SOFT,
+					bits == 2 ? ALEXT.AL_SHORT_SOFT : ALEXT.AL_BYTE_SOFT,
+					(IntPtr) dataPtr
+				);
 #if VERBOSE_AL_DEBUGGING
-			CheckALError();
+				CheckALError();
 #endif
 
-			byte[] monoData = new byte[bufLen / 2];
-			GCHandle monoHandle = GCHandle.Alloc(monoData, GCHandleType.Pinned);
-			IntPtr monoPtr = monoHandle.AddrOfPinnedObject();
-			unsafe
-			{
 				if (bits == 2)
 				{
 					short* src = (short*) dataPtr;
@@ -463,9 +460,6 @@ namespace Microsoft.Xna.Framework.Audio
 					}
 				}
 			}
-			monoHandle.Free();
-			dataHandle.Free();
-			data = null;
 
 			return GenBuffer(
 				monoData,
