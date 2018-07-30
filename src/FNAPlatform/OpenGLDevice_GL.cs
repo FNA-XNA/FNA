b07f85313f4607fc197ee99461501afb37ccb01b
Ethan Lee <flibitijibibo@gmail.com>
2018-07-30 09:38:47 -0400

    OES_draw_elements_base_vertex detection

--- src/FNAPlatform/OpenGLDevice_GL.cs
+++ src/FNAPlatform/OpenGLDevice_GL.cs
@@ -1083,7 +1083,17 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 
 			/* ARB_draw_elements_base_vertex is ideal! */
-			IntPtr ep = SDL.SDL_GL_GetProcAddress("glDrawRangeElementsBaseVertex");
+			IntPtr ep;
+			if (	useES3 &&
+				SDL.SDL_GL_ExtensionSupported("OES_draw_elements_base_vertex") == SDL.SDL_bool.SDL_TRUE	)
+			{
+				/* Android developers are incredibly stupid and export stub functions */
+				ep = SDL.SDL_GL_GetProcAddress("glDrawRangeElementsBaseVertexOES");
+			}
+			else
+			{
+				ep = SDL.SDL_GL_GetProcAddress("glDrawRangeElementsBaseVertex");
+			}
 			supportsBaseVertex = ep != IntPtr.Zero;
 			if (supportsBaseVertex)
 			{
