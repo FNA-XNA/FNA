8b05c2a3b22974e18621a9d0aa8693809e53878b
Ethan Lee <flibitijibibo@gmail.com>
2019-04-01 09:24:14 -0400

    ES does not have PROGRAM_POINT_SIZE

--- src/FNAPlatform/OpenGLDevice.cs
+++ src/FNAPlatform/OpenGLDevice.cs
@@ -809,7 +809,10 @@ namespace Microsoft.Xna.Framework.Graphics
 			else
 			{
 				// Compat-only, but needed for PSIZE0 accuracy
-				glEnable(GLenum.GL_PROGRAM_POINT_SIZE);
+				if (!useES3)
+				{
+					glEnable(GLenum.GL_PROGRAM_POINT_SIZE);
+				}
 				glTexEnvi(GLenum.GL_POINT_SPRITE, GLenum.GL_COORD_REPLACE, 1);
 			}
 		}
