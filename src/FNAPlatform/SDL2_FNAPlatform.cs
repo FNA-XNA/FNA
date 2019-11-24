bd03ca032de0b43953e0f7765c06291722783f5b
Caleb Cornett <caleb.cornett@outlook.com>
2019-11-24 18:10:45 -0500

    Make constants for the GLDevice strings

--- src/FNAPlatform/SDL2_FNAPlatform.cs
+++ src/FNAPlatform/SDL2_FNAPlatform.cs
@@ -44,6 +44,16 @@ namespace Microsoft.Xna.Framework
 
 		#endregion
 
+		#region Graphics Backend String Constants
+
+		private const string OPENGL = "OpenGLDevice";
+		private const string MODERNGL = "ModernGLDevice";
+		private const string THREADEDGL = "ThreadedGLDevice";
+		private const string METAL = "MetalDevice";
+		private const string VULKAN = "VulkanDevice";
+
+		#endregion
+
 		#region Game Objects
 
 		/* This is needed for asynchronous window events */
@@ -204,9 +214,9 @@ namespace Microsoft.Xna.Framework
 		private static bool PrepareGLAttributes()
 		{
 			if (	!String.IsNullOrEmpty(ForcedGLDevice) &&
-				!ForcedGLDevice.Equals("OpenGLDevice") &&
-				!ForcedGLDevice.Equals("ModernGLDevice") &&
-				!ForcedGLDevice.Equals("ThreadedGLDevice")	)
+				!ForcedGLDevice.Equals(OPENGL) &&
+				!ForcedGLDevice.Equals(MODERNGL) &&
+				!ForcedGLDevice.Equals(THREADEDGL)	)
 			{
 				return false;
 			}
@@ -349,24 +359,24 @@ namespace Microsoft.Xna.Framework
 			if (vulkan = PrepareVKAttributes())
 			{
 				initFlags |= SDL.SDL_WindowFlags.SDL_WINDOW_VULKAN;
-				ActualGLDevice = "VulkanDevice";
+				ActualGLDevice = VULKAN;
 			}
 			else if (metal = PrepareMTLAttributes())
 			{
 				// FIXME: SDL_WINDOW_METAL?
-				ActualGLDevice = "MetalDevice";
+				ActualGLDevice = METAL;
 			}
 			else if (opengl = PrepareGLAttributes())
 			{
 				initFlags |= SDL.SDL_WindowFlags.SDL_WINDOW_OPENGL;
-				if (	ForcedGLDevice == "ModernGLDevice" ||
-					ForcedGLDevice == "ThreadedGLDevice"	)
+				if (	ForcedGLDevice == MODERNGL ||
+					ForcedGLDevice == THREADEDGL	)
 				{
 					ActualGLDevice = ForcedGLDevice;
 				}
 				else
 				{
-					ActualGLDevice = "OpenGLDevice";
+					ActualGLDevice = OPENGL;
 				}
 			}
 
@@ -1178,22 +1188,22 @@ namespace Microsoft.Xna.Framework
 		) {
 			switch (ActualGLDevice)
 			{
-				case "OpenGLDevice":
+				case OPENGL:
 					return new OpenGLDevice(presentationParameters, adapter);
 
-				case "ModernGLDevice":
+				case MODERNGL:
 					// FIXME: This is still experimental! -flibit
 					return new ModernGLDevice(presentationParameters, adapter);
 
-				case "ThreadedGLDevice":
+				case THREADEDGL:
 					// FIXME: This is still experimental! -flibit
 					return new ThreadedGLDevice(presentationParameters, adapter);
 
-				case "MetalDevice":
+				case METAL:
 					// Coming soon!
 					break;
 
-				case "VulkanDevice":
+				case VULKAN:
 					// Maybe someday!
 					break;
 			}
