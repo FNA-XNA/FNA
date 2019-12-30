c49e3c5b3d6bdee6c718b6df060a9ce28e82d7c6
Caleb Cornett <caleb.cornett@outlook.com>
2019-12-29 22:31:53 -0500

    SetTextureData3D fix

--- src/FNAPlatform/MetalDevice.cs
+++ src/FNAPlatform/MetalDevice.cs
@@ -3525,9 +3525,9 @@ namespace Microsoft.Xna.Framework.Graphics
 			int h = bottom - top;
 			int d = back - front;
 
-			MTLOrigin origin = new MTLOrigin(left, top, front);
+			MTLOrigin layerOrigin = new MTLOrigin(left, top, 0);
 			MTLSize layerSize = new MTLSize(w, h, 1);
-			MTLRegion layerRegion = new MTLRegion(origin, layerSize);
+			MTLRegion layerRegion = new MTLRegion(layerOrigin, layerSize);
 
 			// Fetch a CPU-accessible texture
 			MetalTexture tex = texture as MetalTexture;
@@ -3559,7 +3559,7 @@ namespace Microsoft.Xna.Framework.Graphics
 					tempHandle,
 					0,
 					level,
-					origin,
+					layerOrigin,
 					layerSize,
 					tex.Handle,
 					0,
