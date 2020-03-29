3cd59654499ae0bdfcfc6f7c5b94385389a4aa76
Ethan Lee <flibitijibibo@gmail.com>
2020-03-29 16:29:33 -0400

    Unset TextureCollection references for disposed textures

--- src/Graphics/Texture.cs
+++ src/Graphics/Texture.cs
@@ -43,6 +43,8 @@ namespace Microsoft.Xna.Framework.Graphics
 		{
 			if (!IsDisposed)
 			{
+				GraphicsDevice.Textures.RemoveDisposedTexture(this);
+				GraphicsDevice.VertexTextures.RemoveDisposedTexture(this);
 				FNA3D.FNA3D_AddDisposeTexture(
 					GraphicsDevice.GLDevice,
 					texture
