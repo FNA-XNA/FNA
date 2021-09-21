fe04c270feac1c508f8b4b2ffda0f4b7a7c0832b
ryancheung <ryancheung.go@gmail.com>
2021-09-21 10:33:45 +0800

    Remove unneeded files

--- abi/Microsoft.Xna.Framework.Storage.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: TypeForwardedToAttribute(typeof(Microsoft.Xna.Framework.Storage.StorageDevice))]
-[assembly: TypeForwardedToAttribute(typeof(Microsoft.Xna.Framework.Storage.StorageContainer))]
-[assembly: TypeForwardedToAttribute(typeof(Microsoft.Xna.Framework.Storage.StorageDeviceNotConnectedException))]
--- abi/Microsoft.Xna.Framework.Video.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: TypeForwardedToAttribute(typeof(Microsoft.Xna.Framework.Media.VideoSoundtrackType))]
-[assembly: TypeForwardedToAttribute(typeof(Microsoft.Xna.Framework.Media.Video))]
-[assembly: TypeForwardedToAttribute(typeof(Microsoft.Xna.Framework.Media.VideoPlayer))]
