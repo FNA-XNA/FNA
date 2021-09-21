6da2a7e7dea8aeeae87069e995c90a0ed6d9f925
ryancheung <ryancheung.go@gmail.com>
2021-09-21 20:59:06 +0800

    Only load SDL2 from local directory, which fixes crash when SDL2 is already installed in os for macOS

--- src/Utilities/NativeLibraryResolver.cs
+++ src/Utilities/NativeLibraryResolver.cs
@@ -53,62 +53,59 @@ namespace Microsoft.Xna.Framework
 				default: return IntPtr.Zero;
 			}
 
-            IntPtr handle;
-
-            var success = NativeLibrary.TryLoad(libraryName, assembly, dllImportSearchPath, out handle);
-            if (success)
-                return handle;
+			IntPtr handle;
+			bool success;
 
 			string rootDirectory = AppContext.BaseDirectory;
 
-            if (OperatingSystem.IsWindows())
-            {
+			if (OperatingSystem.IsWindows())
+			{
 #if WINDOWS10_0_17763_0_OR_GREATER
-                string arch = Environment.Is64BitProcess ? "win10-x64" : "win10-x86";
+				string arch = Environment.Is64BitProcess ? "win10-x64" : "win10-x86";
 #else
-                string arch = Environment.Is64BitProcess ? "win-x64" : "win-x86";
+				string arch = Environment.Is64BitProcess ? "win-x64" : "win-x86";
 #endif
-                var searchPaths = new[]
-                {
-                    // This is where native libraries in our nupkg should end up
-                    Path.Combine(rootDirectory, "runtimes", arch, "native", libraryFileName),
-                    Path.Combine(rootDirectory, Environment.Is64BitProcess ? "x64" : "x86", libraryFileName),
-                    Path.Combine(rootDirectory, libraryFileName)
-                };
+				var searchPaths = new[]
+				{
+					// This is where native libraries in our nupkg should end up
+					Path.Combine(rootDirectory, "runtimes", arch, "native", libraryFileName),
+					Path.Combine(rootDirectory, Environment.Is64BitProcess ? "x64" : "x86", libraryFileName),
+					Path.Combine(rootDirectory, libraryFileName)
+				};
 
-                foreach (var path in searchPaths)
-                {
-                    success = NativeLibrary.TryLoad(path, out handle);
-                    if (success)
-                        return handle;
-                }
+				foreach (var path in searchPaths)
+				{
+					success = NativeLibrary.TryLoad(path, out handle);
+					if (success)
+						return handle;
+				}
 
-                throw new FileLoadException($"Failed to load native library: {libraryName}!");
-            }
+				throw new FileLoadException($"Failed to load native library: {libraryName}!");
+			}
 
-            if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
-            {
-                string arch = OperatingSystem.IsMacOS() ? "osx-x64" : "linux-x64";
+			if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
+			{
+				string arch = OperatingSystem.IsMacOS() ? "osx-x64" : "linux-x64";
 
-                var searchPaths = new[]
-                {
-                    // This is where native libraries in our nupkg should end up
-                    Path.Combine(rootDirectory, "runtimes", arch, "native", libraryFileName),
-                    // The build output folder
-                    Path.Combine(rootDirectory, libraryFileName),
-                    Path.Combine("/usr/local/lib", libraryFileName),
-                    Path.Combine("/usr/lib", libraryFileName)
-                };
+				var searchPaths = new[]
+				{
+					// This is where native libraries in our nupkg should end up
+					Path.Combine(rootDirectory, "runtimes", arch, "native", libraryFileName),
+					// The build output folder
+					Path.Combine(rootDirectory, libraryFileName),
+					Path.Combine("/usr/local/lib", libraryFileName),
+					Path.Combine("/usr/lib", libraryFileName)
+				};
 
-                foreach (var path in searchPaths)
-                {
-                    success = NativeLibrary.TryLoad(path, out handle);
-                    if (success)
-                        return handle;
-                }
+				foreach (var path in searchPaths)
+				{
+					success = NativeLibrary.TryLoad(path, out handle);
+					if (success)
+						return handle;
+				}
 
-                throw new FileLoadException($"Failed to load native library: {libraryName}!");
-            }
+				throw new FileLoadException($"Failed to load native library: {libraryName}!");
+			}
 
 			throw new FileLoadException($"Failed to load native library: {libraryName}!");
 		}
