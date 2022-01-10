b79f56b21a28490095e1b0e259501e2bcf9f0c90
Ethan Lee <flibitijibibo@gmail.com>
2022-01-10 15:15:01 -0500

    Import FNA3D_SupportsBC7

--- src/Graphics/FNA3D.cs
+++ src/Graphics/FNA3D.cs
@@ -791,6 +791,9 @@ namespace Microsoft.Xna.Framework.Graphics
 		public static extern byte FNA3D_SupportsS3TC(IntPtr device);
 
 		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern byte FNA3D_SupportsBC7(IntPtr device);
+
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern byte FNA3D_SupportsHardwareInstancing(
 			IntPtr device
 		);
