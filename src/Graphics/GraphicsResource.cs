f82b11ee90e2ac2836b430aa3a07a9f3699dde99
Katelyn Gadd <kg@luminance.org>
2023-11-11 17:23:49 -0800

    Directly use weak GCHandles instead of WeakReference instances when linking GraphicsResource instances to their GraphicsDevice. We don't need the finalizer offered by the WR class since we have our own finalizer.

--- src/Graphics/GraphicsResource.cs
+++ src/Graphics/GraphicsResource.cs
@@ -10,6 +10,7 @@
 #region Using Statements
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 #endregion
 
 namespace Microsoft.Xna.Framework.Graphics
@@ -39,12 +40,12 @@ namespace Microsoft.Xna.Framework.Graphics
 				if (graphicsDevice != null && selfReference != null)
 				{
 					graphicsDevice.RemoveResourceReference(selfReference);
-					selfReference = null;
+					selfReference.Free();
 				}
 
 				graphicsDevice = value;
 
-				selfReference = new WeakReference(this);
+				selfReference = GCHandle.Alloc(this, GCHandleType.Weak);
 				graphicsDevice.AddResourceReference(selfReference);
 			}
 		}
@@ -79,7 +80,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
 		#region Private Variables
 
-		private WeakReference selfReference;
+		private GCHandle selfReference;
 
 		private GraphicsDevice graphicsDevice;
 
@@ -203,7 +204,7 @@ namespace Microsoft.Xna.Framework.Graphics
 				if (graphicsDevice != null && selfReference != null)
 				{
 					graphicsDevice.RemoveResourceReference(selfReference);
-					selfReference = null;
+					selfReference.Free();
 				}
 
 				IsDisposed = true;
