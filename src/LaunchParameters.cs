258dfa3f657ac5c5098211c20dfada79e448d702
7aGiven <fredrickareichert@gmail.com>
2026-07-19 14:17:57 +0800

    fix LaunchParameters

--- src/LaunchParameters.cs
+++ src/LaunchParameters.cs
@@ -17,28 +17,6 @@ namespace Microsoft.Xna.Framework
 {
 	public class LaunchParameters : Dictionary<string, string>
 	{
-		/* FIXME: This whole parser is one big assumption!
-		 *
-		 * I basically started with what MS programs usually accept as
-		 * arguments, then threw a bunch of values at XNA to see what it
-		 * accepted and what it didn't.
-		 *
-		 * Aside from what you see below, all I could rule out was that
-		 * it doesn't let you do two args as one param, and '=' is not a
-		 * valid value separator either. As an example, "-r:FNA.dll"
-		 * will work, "-r FNA.dll" and "-r=FNA.dll" will not.
-		 *
-		 * The part that bothers me the most, however, is the flag
-		 * indicator. It seems to let anything through as long as : is
-		 * there, but it trims some special chars, and does so pretty
-		 * broadly. You can do '-', "--", "---", etc! Lastly, in
-		 * addition to the chars below, I also tried '+', which didn't
-		 * work. I have no idea if there are any other chars to check.
-		 *
-		 * If anybody has an official standard, I'd like to see it!
-		 * -flibit
-		 */
-
 		#region Private Static Variables
 
 		private static readonly char[] flags = new char[]
@@ -53,29 +31,25 @@ namespace Microsoft.Xna.Framework
 		public LaunchParameters()
 		{
 			string[] args = System.Environment.GetCommandLineArgs();
-			foreach (string a in args)
+			for (int i = 1; i < args.Length; i++)
 			{
-				string arg = a.TrimStart(flags);
-
-				/* 1 for ':', 1 for key, 1 for value */
-				if (arg.Length < 3)
-				{
-					continue;
-				}
+				string key = args[i].TrimStart(flags);
+				string value = string.Empty;
 
 				/* You can have multiple :, only the first matters */
-				int valueOffset = arg.IndexOf(":", 1, arg.Length - 2);
-				if (valueOffset >= 0)
+				int valueOffset = key.IndexOf(":");
+				if (valueOffset != -1)
 				{
-					/* All instances after the first are ignored */
-					string key = arg.Substring(0, valueOffset);
-					if (!ContainsKey(key))
-					{
-						Add(
-							key,
-							arg.Substring(valueOffset + 1)
-						);
-					}
+					value = key.Substring(valueOffset + 1);
+					key = key.Substring(0, valueOffset);
+				}
+				/* All instances after the first are ignored */
+				if (key.Length != 0 && !ContainsKey(key))
+				{
+					Add(
+						key,
+						value
+					);
 				}
 			}
 		}
