440c0227282dfebef41c77354b7496bc0e754f45
7aGiven <fredrickareichert@gmail.com>
2026-07-21 09:00:32 +0800

    append class to MathTypeConverter.cs

--- src/Design/MathTypeConverter.cs
+++ src/Design/MathTypeConverter.cs
@@ -11,7 +11,7 @@
 using System;
 using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
-
+using System.Reflection;
 #endregion
 
 namespace Microsoft.Xna.Framework.Design
@@ -63,5 +63,99 @@ namespace Microsoft.Xna.Framework.Design
 		}
 
 		#endregion
+	}
+
+	internal abstract class MemberPropertyDescriptor : PropertyDescriptor
+	{
+		private readonly MemberInfo member;
+
+		public MemberPropertyDescriptor(MemberInfo member) : base(member.Name, (Attribute[]) member.GetCustomAttributes(typeof(Attribute), true))
+		{
+			this.member = member;
+		}
+
+		public override Type ComponentType
+		{
+			get
+			{
+				return member.DeclaringType;
+			}
+		}
+
+		public override bool IsReadOnly
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public override bool CanResetValue(object component)
+		{
+			return false;
+		}
+
+		public override void ResetValue(object component) { }
+
+		public override bool ShouldSerializeValue(object component)
+		{
+			return true;
+		}
+	}
+
+	internal class FieldPropertyDescriptor : MemberPropertyDescriptor
+	{
+		private readonly FieldInfo field;
+
+		public FieldPropertyDescriptor(FieldInfo field) : base(field)
+		{
+			this.field = field;
+		}
+
+		public override Type PropertyType
+		{
+			get
+			{
+				return field.FieldType;
+			}
+		}
+
+		public override object GetValue(object component)
+		{
+			return field.GetValue(component);
+		}
+
+		public override void SetValue(object component, object value)
+		{
+			field.SetValue(component, value);
+		}
+	}
+
+	internal class PropertyPropertyDescriptor : MemberPropertyDescriptor
+	{
+		private readonly PropertyInfo property;
+
+		public PropertyPropertyDescriptor(PropertyInfo property) : base(property)
+		{
+			this.property = property;
+		}
+
+		public override Type PropertyType
+		{
+			get
+			{
+				return property.PropertyType;
+			}
+		}
+
+		public override object GetValue(object component)
+		{
+			return property.GetValue(component, null);
+		}
+
+		public override void SetValue(object component, object value)
+		{
+			property.SetValue(component, value, null);
+		}
 	}
 }
