da4879a6622ac8316eb3985dcb7779cfe6b785b0
7aGiven <fredrickareichert@gmail.com>
2026-07-22 19:11:23 +0800

    fix indent

--- src/Design/MatrixConverter.cs
+++ src/Design/MatrixConverter.cs
@@ -62,16 +62,16 @@ namespace Microsoft.Xna.Framework.Design
 					Matrix matrix = (Matrix) value;
 					return new InstanceDescriptor(
 						typeof(Matrix).GetConstructor(new Type[] {
-						typeof(float), typeof(float), typeof(float), typeof(float),
-						typeof(float), typeof(float), typeof(float), typeof(float),
-						typeof(float), typeof(float), typeof(float), typeof(float),
-						typeof(float), typeof(float), typeof(float), typeof(float)
+							typeof(float), typeof(float), typeof(float), typeof(float),
+							typeof(float), typeof(float), typeof(float), typeof(float),
+							typeof(float), typeof(float), typeof(float), typeof(float),
+							typeof(float), typeof(float), typeof(float), typeof(float)
 						}),
 						new float[] {
-						matrix.M11, matrix.M12, matrix.M13, matrix.M14,
-						matrix.M21, matrix.M22, matrix.M23, matrix.M24,
-						matrix.M31, matrix.M32, matrix.M33, matrix.M34,
-						matrix.M41, matrix.M42, matrix.M43, matrix.M44
+							matrix.M11, matrix.M12, matrix.M13, matrix.M14,
+							matrix.M21, matrix.M22, matrix.M23, matrix.M24,
+							matrix.M31, matrix.M32, matrix.M33, matrix.M34,
+							matrix.M41, matrix.M42, matrix.M43, matrix.M44
 						}
 					);
 				}
