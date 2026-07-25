33bcc6cd9808f88ecdf657490bd1eb240b2a3b2d
7aGiven <fredrickareichert@gmail.com>
2026-07-26 07:15:38 +0800

    iPad support more Touch Point

--- src/Input/Touch/TouchCollection.cs
+++ src/Input/Touch/TouchCollection.cs
@@ -80,7 +80,7 @@ namespace Microsoft.Xna.Framework.Input.Touch
 			{
 				throw new ArgumentNullException("touches");
 			}
-			if (touches.Length > 10) // XNA: touches.Length > 8
+			if (touches.Length > 17) // XNA: touches.Length > 8
 			{
 				throw new ArgumentOutOfRangeException("touches");
 			}
