fb1a2518c04c904ba5fd7146671fe5edc057e02f
7aGiven <fredrickareichert@gmail.com>
2026-09-17 10:09:31 +0800

    exception accuracy: check data length == 0 for GetData and SetData in Texture

--- src/Graphics/Texture2D.cs
+++ src/Graphics/Texture2D.cs
@@ -173,9 +173,9 @@ namespace Microsoft.Xna.Framework.Graphics
 			{
 				throw new ObjectDisposedException(GetType().Name);
 			}
-			if (data == null)
+			if (data == null || data.Length == 0)
 			{
-				throw new ArgumentNullException("data");
+				throw new ArgumentNullException("data", "This method does not accept null for this parameter.");
 			}
 			if (unchecked((uint) level >= (uint) LevelCount))
 			{
@@ -310,7 +310,7 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
 			if (data == null || data.Length == 0)
 			{
-				throw new ArgumentException("data cannot be null");
+				throw new ArgumentNullException("data", "This method does not accept null for this parameter.");
 			}
 			if (unchecked((uint) level >= (uint) LevelCount))
 			{
