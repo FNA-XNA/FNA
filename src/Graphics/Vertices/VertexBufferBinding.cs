bd8f73834a18ef417134a32c1115217db493fec1
7aGiven <fredrickareichert@gmail.com>
2026-09-19 18:59:53 +0800

    exception accuracy: implicit for VertexBufferBinding

--- src/Graphics/Vertices/VertexBufferBinding.cs
+++ src/Graphics/Vertices/VertexBufferBinding.cs
@@ -137,16 +137,7 @@ namespace Microsoft.Xna.Framework.Graphics
 
 		public static implicit operator VertexBufferBinding(VertexBuffer buffer)
 		{
-			/* Unlike the constructors, we have to be ready for buffer to be null
-			 * due to weird left-hand assignment shenanigans. The one that tripped
-			 * me was SetVertexBuffer calling this for some odd reason.
-			 * -flibit
-			 */
-			VertexBufferBinding result;
-			result.vertexBuffer = buffer;
-			result.vertexOffset = 0;
-			result.instanceFrequency = 0;
-			return result;
+			return new VertexBufferBinding(buffer);
 		}
 
 		#endregion
